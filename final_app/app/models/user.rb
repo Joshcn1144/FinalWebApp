39ff43c37ed7cfe5a2ec57a2a11734644cf18555
Joshua <jcerva21@msudenver.edu>
2024-11-19 12:27:57 -0700

    not working

--- final_app/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-    # my user will have a one to many relationship with menus
-    has_many :menus, dependent: :destroy
-
-    #add validations and profile pic
-end
